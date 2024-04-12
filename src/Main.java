6d3b28e96fe5e13712cd041141006fef73067b5c
oneliker <154754396+oneliker@users.noreply.github.com>
2024-04-12 09:43:07 +0500

    first

--- src/Main.java
+++ src/Main.java
@@ -140,3 +140,16 @@ public class Main {
         scanner.close();
     }
 }
+//import java.util.Scanner;
+//
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner  = new scanner (Sistem.in);
+//        String word = scanner.nextLine();
+//        int count = word.length();
+//        System.out.printf((count));
+//
+//    }
+//}
